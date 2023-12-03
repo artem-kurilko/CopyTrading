d11ad9c196839dcd7a0a433cd6a788d60f7dc900
Artem <kurilko365@gmail.com>
2023-12-03 20:25:11 +0200

    feat: remove redundant

--- tradewagon/src/main/java/com/copytrading/tradewagon/gmail/RealTimeEmailReader.java
+++ tradewagon/src/main/java/com/copytrading/tradewagon/gmail/RealTimeEmailReader.java
@@ -1,47 +1,62 @@
 package com.copytrading.tradewagon.gmail;
 
 import javax.mail.*;
+import javax.mail.event.MessageCountEvent;
+import javax.mail.event.MessageCountListener;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Properties;
-import javax.mail.event.MessageCountListener;
-import javax.mail.event.MessageCountEvent;
-
-import com.sun.mail.imap.IdleManager;
-import lombok.Value;
 
 public class RealTimeEmailReader {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MessagingException {
         final String HOST = "imap.gmail.com";
         String username = getLocalProperties().getProperty("gmail.username");
         String password = getLocalProperties().getProperty("gmail.password");
 
-        Properties properies = new Properties();
-        properies.put("mail.smtp.host", "smtp.gmail.com");
-        properies.put("mail.smtp.port", "465");
-        properies.put("mail.smtp.auth", "true");
-        properies.put("mail.smtp.starttls.enable", "true");
-        properies.put("mail.smtp.starttls.required", "true");
-        properies.put("mail.smtp.ssl.protocols", "TLSv1.2");
-        properies.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-     /*   properies.put("mail.store.protocol", "imaps");
-        properies.put("mail.imaps.host", HOST);
-        properies.put("mail.imaps.port", "993");
-        properies.put("mail.imaps.ssl.enable", "true");
-        properies.put("mail.smtp.starttls.required", "true");
-        properies.put("mail.smtp.ssl.protocols", "TLSv1.2");*/
+        Store store = Session.getDefaultInstance(getProperties()).getStore("imaps");
+        store.connect(HOST, username, password);
 
-        try {
-            Session session = Session.getDefaultInstance(properies);
+        Folder folder = store.getFolder("inbox");
+
+        folder.addMessageCountListener(new MessageCountListener() {
+            @Override
+            public void messagesAdded(MessageCountEvent messageCountEvent) {
+                System.out.println("CALLED MESSAGES ADDED");
+                Message[] messages = messageCountEvent.getMessages();
+                for (Message s : messages) {
+                    try {
+                        System.out.println("New email: " + s.getSubject());
+                    } catch (MessagingException e) {
+                        throw new RuntimeException(e); //todo
+                    }
+                }
+            }
+
+            @Override
+            public void messagesRemoved(MessageCountEvent messageCountEvent) {
+                System.out.println("Message removed");
+            }
+        });
+
+        // Бесконечный цикл для ожидания событий
+        while (true) {
+            try {
+                Thread.sleep(1); // Приостанавливаем выполнение на 1 секунду
+            } catch (InterruptedException e) {
+                // Обработка ошибок
+            }
+        }
+
+       /* try {
+            Session session = Session.getDefaultInstance(getProperties());
             Store store = session.getStore("imaps");
             store.connect(HOST, username, password);
 
             // Open the inbox folder in read/write mode
             Folder inbox = store.getFolder("INBOX");
             inbox.open(Folder.READ_WRITE);
-            System.out.println("Finished");
             MessageCountListener messageCountListener = new MessageCountListener() {
                 @Override
                 public void messagesAdded(MessageCountEvent event) {
@@ -51,6 +66,7 @@ public class RealTimeEmailReader {
 
                         for (Message message : messages) {
                             System.out.println("New Email Subject: " + message.getSubject());
+                            System.out.println("New Email Text: " + message);
                         }
                     } catch (MessagingException e) {
                         e.printStackTrace();
@@ -75,7 +91,19 @@ public class RealTimeEmailReader {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
+    }
+
+    private static Properties getProperties() {
+        Properties properies = new Properties();
+        properies.put("mail.smtp.host", "smtp.gmail.com");
+        properies.put("mail.smtp.port", "465");
+        properies.put("mail.smtp.auth", "true");
+        properies.put("mail.smtp.starttls.enable", "true");
+        properies.put("mail.smtp.starttls.required", "true");
+        properies.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        properies.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        return properies;
     }
 
     private static Properties getLocalProperties() {
